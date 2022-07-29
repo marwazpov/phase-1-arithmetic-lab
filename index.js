1de5980506af0f3b795e4ee65770b242b0b9340e
marwa <mds08@mail.aub.edu>
2022-07-29 20:40:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1=2;
+let num2=31;
+let multiply = num1*num2;
 
-// Write your code here
+let random= Math.floor(Math.random() + 1);
+
+
+let num3 = 4;
+let num4 = 5;
+let mod;
+mod = num3 % num4;
+
+
+let max= Math.max(0,20);
